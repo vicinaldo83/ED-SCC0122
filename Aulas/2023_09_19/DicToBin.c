d2ec1425812a7d26bd711df8c54381fd8764208e
vicinaldo83 <higormatosvici@gmail.com>
2023-09-25 17:15:15 -0300

    Aula semana 17_09 a 23_09

---
+++ Aulas/2023_09_19/DicToBin.c
@@ -0,0 +1,48 @@
+/*DicToBin.c*/
+
+#include <stdio.h>
+/*
+    #define PilhaSize 100
+
+    typedef int items
+    typdef int topo
+
+    struct { 
+        
+    }
+
+*/
+
+
+/*  
+    import Pilha
+    main {
+        pilha = Pilha();
+        int decimal = input();
+
+        while (decimal > 0){
+            pilha.push(decimal % 2);
+            decimal = decimal / 2;
+        }
+
+        print(pilha.flatten());
+        pilha.destroy();
+    }
+
+    pilha.push():... adiciona item a pilha
+    pilha.faltten() {
+        out
+        while (!pilha.vazia()) {
+            out += (str)pilha.pop()
+        }
+    }
+    pilha.pop():.. retorna e remove primeiro item
+    pilha.destroy():... autoexplicativo
+    pilha.vaiza or size:... verifica o tamnho da lista
+*/
+
+
+
+int main() {
+
+}
